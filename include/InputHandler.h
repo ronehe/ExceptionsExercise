f20809b737b883c55e63381417ad3c266ca505a2
ronehe <Ro.Heifetz@gmail.com>
2021-05-06 07:18:44 +0300

    input handler now works with cin

--- include/InputHandler.h
+++ include/InputHandler.h
@@ -8,18 +8,24 @@ typedef std::istream streamObj;
 class InputHandler {
 public:
 	InputHandler(streamObj& istr);
+	~InputHandler();
 	void addStream(streamObj& istr);
-	void removeStream();
-	void readNewLine();
+	void removeStream() const;
+	void readNewLine() const;
 	void handleEOF();
 	template<typename T>
 	friend void operator>>(const InputHandler&, T&);
 private:
-	std::stack<streamObj*> m_streams;
-	std::stringstream *m_line;
+	std::stack<streamObj*>* m_streams;
+	std::stringstream* m_line;
 };
 /// global operator >>  for handling the top of the stack which is the current stream  
 template<typename T>
 void operator>>(const InputHandler&cur, T &into ){
 	*(cur.m_line) >> into;
+	if (!(*cur.m_line)) {
+		cur.m_line->clear();
+		cur.readNewLine();
+		*(cur.m_line) >> into;
+	}
 }
--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -1,30 +1,40 @@
 #include <InputHandler.h>
 InputHandler::InputHandler(streamObj& istr) {
+	m_streams = new std::stack<streamObj*>;
+	m_line = new std::stringstream();
 	addStream(istr);
 	//readNewLine();
 }
 void InputHandler::addStream(streamObj& istr) {
 	istr.exceptions(std::ios::eofbit);
-	m_streams.push(&istr);
+	m_streams->push(&istr);
 }
 
-void InputHandler::removeStream() {
-	delete m_streams.top();
-	m_streams.pop();
+void InputHandler::removeStream() const {
+	if (auto p = dynamic_cast<std::ifstream*>(m_streams->top())) {
+		p->close();
+	}
+	delete m_streams->top();
+	m_streams->pop();
 }
 
-void InputHandler::readNewLine() {
+void InputHandler::readNewLine() const{
 	auto temp = std::string();
-	if (m_streams.top()->eof()) {
+	if (!(*(m_streams->top()))) {
 		removeStream();
 	}
-	std::getline(*(m_streams.top()), temp);
-	*m_line << temp;
+	std::getline(*(m_streams->top()), temp);
+	m_line->str(temp);
 }
 
 void InputHandler::handleEOF() {
 	removeStream();
 	auto temp = std::string();
-	std::getline(*m_streams.top(), temp);
-	*m_line << temp;
+	std::getline(*(m_streams->top()), temp);
+	(*m_line) << temp;
+}
+
+InputHandler::~InputHandler() {
+	delete m_line;
+	delete m_streams;
 }
